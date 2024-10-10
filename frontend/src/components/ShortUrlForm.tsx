f33838bbd67a258c4e3941b6d0787fe8314d7af2
JeremyLoh <jeremyloh03@gmail.com>
2024-10-10 22:29:30 +0800

    feat: add frontend form for getting url

---
+++ frontend/src/components/ShortUrlForm.tsx
@@ -0,0 +1,56 @@
+import { SubmitHandler, useForm } from "react-hook-form"
+import "./ShortUrlForm.css"
+
+type FormFields = {
+  url: string
+}
+
+function ShortUrlForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>()
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    // TODO call backend to get short url
+    console.log(data)
+  }
+  function isValidUrl(value: string) {
+    const errorMessage =
+      "Please provide a valid url starting with http: or https:"
+    let url
+    try {
+      url = new URL(value)
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      return errorMessage
+    }
+    return url.protocol === "http:" || url.protocol === "https:" || errorMessage
+  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="short-url-form">
+      <label htmlFor="url">Url</label>
+      <input
+        id="url"
+        aria-invalid={errors.url ? "true" : "false"}
+        className={errors.url ? `error-border` : ""}
+        {...register("url", {
+          required: "Url is required",
+          minLength: { value: 3, message: "Min length is 3" },
+          maxLength: { value: 2048, message: "Max length is 2048" },
+          validate: {
+            validateUrl: isValidUrl,
+          },
+        })}
+      />
+      {errors.url && (
+        <p role="alert" className="error-text">
+          {errors.url.message}
+        </p>
+      )}
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default ShortUrlForm
