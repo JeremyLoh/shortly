328cee58c7db0d091fd9f0125ced4934e8621d51
JeremyLoh <jeremyloh03@gmail.com>
2024-10-12 00:07:35 +0800

    refactor: short url card style

--- frontend/src/components/ShortUrl.tsx
+++ frontend/src/components/ShortUrl.tsx
@@ -4,13 +4,27 @@ import { Url } from "../endpoints/createUrl"
 function ShortUrl({ url }: { url: Url }) {
   return (
     <div className="card">
-      <h2 className="text-right">Your New Short Url</h2>
-      <p className="text-right">
-        Short Code{" "}
-        <a href={`/${url.shortCode}`} target="_blank">
-          {url.shortCode}
-        </a>
-      </p>
+      <div>
+        <h2 className="text-center">Your New Short Url 🎯</h2>
+        <p className="text-center">
+          Short Url{" "}
+          <a href={`/${url.shortCode}`} target="_blank">
+            {`${window.location.href}${url.shortCode}`}
+          </a>
+          <br />
+          <button
+            className="copy-short-url-btn"
+            onClick={() =>
+              navigator.clipboard.writeText(
+                `${window.location.href}${url.shortCode}`
+              )
+            }
+          >
+            Copy Link
+          </button>
+        </p>
+      </div>
+      <hr className="divider" />
       <p className="text-right">
         Redirect Url{" "}
         <a href={url.url} target="_blank">
