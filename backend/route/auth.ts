625a4c106f0df0f49f270c988a461bd66c957e05
JeremyLoh <jeremyloh03@gmail.com>
2024-10-14 21:58:56 +0800

    feat: add backend create user endpoint and request validation

--- backend/route/auth.ts
+++ backend/route/auth.ts
@@ -1,8 +1,13 @@
-import { Request, Response, Router } from "express"
-import passport from "passport"
 import "../auth/strategy/local-strategy.js"
+import passport from "passport"
+import { Request, Response, Router } from "express"
+import { checkSchema, matchedData, validationResult } from "express-validator"
+import { createUserValidationSchema } from "../validation/schema.js"
+import { createUser } from "../model/user.js"
 
 const router = Router()
+
+// TODO Rate limit endpoints
 
 router.post("/api/auth", passport.authenticate("local"), (req, res) => {
   // TODO login if auth is proper (username and password)
@@ -22,5 +27,24 @@ router.post("/api/auth/logout", (req: Request, res: Response) => {
     }
   })
 })
+
+router.post(
+  "/api/auth/users",
+  checkSchema(createUserValidationSchema(), ["body"]),
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      res.status(400).send({ error: errors.array().map((e) => e.msg) })
+      return
+    }
+    const { username, password } = matchedData(req)
+    try {
+      const newUser = await createUser(username, password)
+      res.status(201).send(newUser)
+    } catch (error: any) {
+      res.status(400).send({ error: error.message })
+    }
+  }
+)
 
 export default router
--- backend/validation/schema.ts
+++ backend/validation/schema.ts
@@ -30,4 +30,32 @@ function createShortCodeValidationSchema() {
   }
 }
 
-export { createUrlValidationSchema, createShortCodeValidationSchema }
+function createUserValidationSchema() {
+  return {
+    username: {
+      isLength: {
+        options: { min: 1, max: 255 },
+        errorMessage:
+          "Please provide a valid username (min length 1, max length 255)",
+      },
+      isString: {
+        errorMessage: "Username must be a string",
+      },
+      notEmpty: {
+        errorMessage: "Username cannot be empty",
+      },
+    },
+    password: {
+      isLength: {
+        options: { min: 8 },
+        errorMessage: "Password must be at least 8 characters long",
+      },
+    },
+  }
+}
+
+export {
+  createUrlValidationSchema,
+  createShortCodeValidationSchema,
+  createUserValidationSchema,
+}
