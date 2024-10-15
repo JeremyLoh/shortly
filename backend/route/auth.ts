55e4345b34cb479814a5bee1890571251a638dca
JeremyLoh <jeremyloh03@gmail.com>
2024-10-15 20:12:37 +0800

    feat: add rate limit on backend auth endpoints

--- backend/route/auth.ts
+++ backend/route/auth.ts
@@ -4,32 +4,60 @@ import { Request, Response, Router } from "express"
 import { checkSchema, matchedData, validationResult } from "express-validator"
 import { createUserValidationSchema } from "../validation/schema.js"
 import { createUser } from "../model/user.js"
+import rateLimit from "express-rate-limit"
 
 const router = Router()
 
-// TODO Rate limit endpoints
-
-router.post("/api/auth/login", passport.authenticate("local"), (req, res) => {
-  // login and get cookie if auth is proper (username and password)
-  res.sendStatus(200)
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  limit: 3,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
 })
-
-// @ts-ignore
-router.post("/api/auth/logout", (req: Request, res: Response) => {
-  if (!req.user) {
-    return res.sendStatus(401)
-  }
-  req.logout((error) => {
-    if (error) {
-      res.sendStatus(400)
-    } else {
-      res.sendStatus(200)
-    }
-  })
+const logoutLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  limit: 3,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+})
+const createUserLimiter = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000, // 1 day
+  limit: 2,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
 })
 
 router.post(
+  "/api/auth/login",
+  loginLimiter,
+  passport.authenticate("local"),
+  (req, res) => {
+    // login and get cookie if auth is proper (username and password)
+    res.sendStatus(200)
+  }
+)
+
+router.post(
+  "/api/auth/logout",
+  logoutLimiter,
+  // @ts-ignore
+  (req: Request, res: Response) => {
+    if (!req.user) {
+      return res.sendStatus(401)
+    }
+    req.logout((error) => {
+      if (error) {
+        res.sendStatus(400)
+      } else {
+        res.sendStatus(200)
+      }
+    })
+  }
+)
+
+router.post(
   "/api/auth/users",
+  createUserLimiter,
   checkSchema(createUserValidationSchema(), ["body"]),
   async (req: Request, res: Response) => {
     const errors = validationResult(req)
