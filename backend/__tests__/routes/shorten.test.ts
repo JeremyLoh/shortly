379e62103cf23bb476e5a69cea71147f58859423
JeremyLoh <jeremyloh03@gmail.com>
2024-10-17 22:33:54 +0800

    feat: add more negative tests for creating short url

--- backend/__tests__/routes/shorten.test.ts
+++ backend/__tests__/routes/shorten.test.ts
@@ -42,23 +42,92 @@ describe("Shorten Url API", () => {
     vi.restoreAllMocks()
   })
 
-  test("POST /api/shorten should create new shorten url", async () => {
-    const expectedUrl = "http://example.com"
-    const response = await request(app)
-      .post("/api/shorten")
-      .send({ url: expectedUrl })
-      .set("Accept", "application/json")
-    expect(response.status).toBe(201)
-    expect(response.body).toEqual(
-      expect.objectContaining({
-        id: expect.any(String),
-        shortCode: expect.any(String),
-        createdAt: expect.any(String),
-        updatedAt: null,
-        url: expectedUrl,
-      })
-    )
-    expect(response.body.shortCode).toHaveLength(7)
-    expect(new Date(response.body.createdAt).getTime()).not.toBeNaN()
+  describe("POST /api/shorten", () => {
+    test("should create new shorten url", async () => {
+      const expectedUrl = "http://example.com"
+      const response = await request(app)
+        .post("/api/shorten")
+        .send({ url: expectedUrl })
+        .set("Accept", "application/json")
+      expect(response.status).toBe(201)
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          shortCode: expect.any(String),
+          createdAt: expect.any(String),
+          updatedAt: null,
+          url: expectedUrl,
+        })
+      )
+      expect(response.body.shortCode).toHaveLength(7)
+      expect(new Date(response.body.createdAt).getTime()).not.toBeNaN()
+    })
+
+    test("missing url in request body should not create new shorten url", async () => {
+      const response = await request(app)
+        .post("/api/shorten")
+        .send({})
+        .set("Accept", "application/json")
+      expect(response.status).toBe(400)
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          error: expect.arrayContaining([
+            "Url length should be between 3 and 2048",
+          ]),
+        })
+      )
+    })
+
+    test("invalid url protocol in request body should not create new shorten url", async () => {
+      const invalidUrlProtocol = "htp://test.co"
+      const response = await request(app)
+        .post("/api/shorten")
+        .send({ url: invalidUrlProtocol })
+        .set("Accept", "application/json")
+      expect(response.status).toBe(400)
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          error: "Please provide a valid http / https url",
+        })
+      )
+    })
+
+    test("max valid url length in request body should create new shorten url", async () => {
+      const expectedLongUrl =
+        "http://" + "a".repeat(2048 - "http://.com".length) + ".com"
+      const response = await request(app)
+        .post("/api/shorten")
+        .send({ url: expectedLongUrl })
+        .set("Accept", "application/json")
+      expect(response.status).toBe(201)
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          shortCode: expect.any(String),
+          createdAt: expect.any(String),
+          updatedAt: null,
+          url: expectedLongUrl,
+        })
+      )
+      expect(response.body.shortCode).toHaveLength(7)
+      expect(new Date(response.body.createdAt).getTime()).not.toBeNaN()
+    })
+
+    test("max invalid url length in request body should not create new shorten url", async () => {
+      const expectedLongUrl =
+        "http://" + "a".repeat(2049 - "http://.com".length) + ".com"
+      const response = await request(app)
+        .post("/api/shorten")
+        .send({ url: expectedLongUrl })
+        .set("Accept", "application/json")
+      expect(response.status).toBe(400)
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          error: expect.arrayContaining([
+            "Url length should be between 3 and 2048",
+          ]),
+        })
+      )
+    })
   })
 })
