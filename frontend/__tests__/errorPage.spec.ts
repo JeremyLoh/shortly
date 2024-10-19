f3ddf3c164e4411cf1705bff264fdb5e9599ae57
JeremyLoh <jeremyloh03@gmail.com>
2024-10-19 21:27:32 +0800

    feat: add frontend error route tests

---
+++ frontend/__tests__/errorPage.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test"
+
+const HOMEPAGE_URL = "localhost:5173"
+
+function assertErrorPageShown(page) {
+  expect(page.getByText("Error - 404 Not Found")).toBeVisible()
+  expect(page.getByText("An error has occured, to continue:")).toBeVisible()
+  expect(page.getByText("Return to the homepage.")).toBeVisible()
+  expect(page.getByText("Try again later.")).toBeVisible()
+}
+
+test("invalid route shows 404 error page", async ({ page }) => {
+  const invalidRoute = HOMEPAGE_URL + "/12abc67/info"
+  await page.goto(invalidRoute)
+  expect(page.url()).toContain("/12abc67/info")
+  assertErrorPageShown(page)
+})
+
+test("short code that does not exist shows 404 error page", async ({
+  page,
+}) => {
+  const invalidShortCodeRoute = HOMEPAGE_URL + "/12abc67"
+  await page.goto(invalidShortCodeRoute)
+  expect(page.url()).toContain("/12abc67")
+  assertErrorPageShown(page)
+})
+
+test("short code invalid length shows 404 error page", async ({ page }) => {
+  const invalidLengthShortCodeRoute = HOMEPAGE_URL + "/ab123"
+  await page.goto(invalidLengthShortCodeRoute)
+  expect(page.url()).toContain("/ab123")
+  assertErrorPageShown(page)
+})
