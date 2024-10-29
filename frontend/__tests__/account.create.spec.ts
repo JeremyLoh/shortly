c45f8f9d8a833827549aa3bf9c38771910935492
JeremyLoh <jeremyloh03@gmail.com>
2024-10-29 22:57:03 +0800

    feat: add create account username error and root error message

--- frontend/__tests__/account.create.spec.ts
+++ frontend/__tests__/account.create.spec.ts
@@ -12,6 +12,21 @@ async function mockLoginEndpoint(page: Page) {
   })
 }
 
+async function mockLoginRateLimitExceededEndpoint(
+  page: Page,
+  timeoutInSeconds: number
+) {
+  await page.route("*/**/api/auth/users", async (route) => {
+    const request = route.request()
+    expect(request.method()).toBe("POST")
+    await route.fulfill({
+      status: 429,
+      headers: { "retry-after": `${timeoutInSeconds}` },
+      json: "Too many requests, please try again later.",
+    })
+  })
+}
+
 test.beforeEach(async ({ page }) => {
   await page.goto(HOMEPAGE_URL + "/register")
   expect(page.url()).toContain("/register")
@@ -44,4 +59,44 @@ test("should create new account", async ({ page }) => {
     page.getByRole("heading", { name: "Login", exact: true })
   ).toBeVisible()
   expect(page.url()).toContain("/login")
+})
+
+test("should display error when username is blank", async ({ page }) => {
+  await page.getByLabel("Username").clear()
+  await page.getByRole("button", { name: "Create Account" }).click()
+  await expect(page.getByText("Username is required")).toBeVisible()
+})
+
+test("should display error when username is more than 255 characters", async ({
+  page,
+}) => {
+  const username = "a".repeat(256)
+  await page.getByLabel("Username").fill(username)
+  await page.getByRole("button", { name: "Create Account" }).click()
+  await expect(
+    page.getByText("Username cannot be longer than 255 characters")
+  ).toBeVisible()
+
+  await page.getByLabel("Username").clear()
+  await page.getByLabel("Username").fill("b".repeat(255))
+  await expect(
+    page.getByText("Username cannot be longer than 255 characters")
+  ).not.toBeVisible()
+})
+
+test("should display root error message when submission fails with rate limit exception", async ({
+  page,
+}) => {
+  const timeoutInSeconds = 30
+  await mockLoginRateLimitExceededEndpoint(page, timeoutInSeconds)
+
+  await page.getByLabel("Username").fill("test_username")
+  await page.getByLabel("Password", { exact: true }).fill("test_password")
+  await page.getByLabel("Confirm Password").fill("test_password")
+  await page.getByRole("button", { name: "Create Account" }).click()
+  await expect(
+    page.getByText(
+      `Rate Limit Exceeded, please try again after ${timeoutInSeconds} seconds`
+    )
+  ).toBeVisible()
 })
--- frontend/src/components/CreateAccountForm.tsx
+++ frontend/src/components/CreateAccountForm.tsx
@@ -15,21 +15,40 @@ function CreateAccountForm({ handleCreateAccount }: CreateAccountProps) {
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
+    setError,
+    formState: { errors, isSubmitting },
   } = useForm<FormFields>()
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
     try {
       await handleCreateAccount(data.username, data.password)
-    } catch (error) {
-      // TODO display error in form
-      console.error(error)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      setError("root", {
+        message: error.message,
+      })
     }
   }
   return (
     <form id="create-account-form" onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="username">Username</label>
-      <input id="username" type="text" {...register("username")} />
-
+      <input
+        id="username"
+        type="text"
+        {...register("username", {
+          required: "Username is required",
+          maxLength: {
+            value: 255,
+            message: "Username cannot be longer than 255 characters",
+          },
+        })}
+      />
+      {errors.username && (
+        <div>
+          <p role="alert" className="error-text">
+            {errors.username.message}
+          </p>
+        </div>
+      )}
       <label htmlFor="password">Password</label>
       <input id="password" type="password" {...register("password")} />
 
@@ -39,6 +58,14 @@ function CreateAccountForm({ handleCreateAccount }: CreateAccountProps) {
         type="password"
         {...register("confirmPassword")}
       />
+
+      {errors.root && (
+        <div>
+          <p role="alert" className="error-text">
+            {errors.root.message}
+          </p>
+        </div>
+      )}
       <button type="submit" disabled={isSubmitting}>
         Create Account
       </button>
