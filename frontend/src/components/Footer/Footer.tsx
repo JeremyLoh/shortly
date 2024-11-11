cc5ad50b2d425ec4b9ca0b0838dd2404344fae23
JeremyLoh <jeremyloh03@gmail.com>
2024-11-11 22:09:38 +0800

    feat: add kofi support button in footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,16 +1,30 @@
 import { Link } from "react-router-dom"
 import "./Footer.css"
+import Kofi from "/public/kofi1.webp"
 
 function Footer() {
   return (
     <footer id="footer">
-      <span>Made by </span>
       <Link
+        id="footer-github-link"
         to="https://github.com/JeremyLoh/"
         target="_blank"
         rel="noopener noreferrer"
       >
         @Jeremy_Loh
+      </Link>
+      <Link
+        id="footer-kofi-button"
+        to="https://ko-fi.com/W7W215XL1U"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          height="36"
+          style={{ border: "0px", height: "36px" }}
+          src={Kofi}
+          alt="Buy Me a Coffee at ko-fi.com"
+        />
       </Link>
     </footer>
   )
